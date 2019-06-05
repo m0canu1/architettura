cf85c2e313d5df1ab88974da60bce0f35c482f07
m0canu1 <alexandrumocanu9@gmail.com>
2019-06-05 12:01:42 +0200

    aa

---
+++ laboratorio/ijvm/Calcola2.java
@@ -0,0 +1,12 @@
+public class Calcola2 {
+	public static void main (String[] args)
+   		{
+   			int i, j=6, k=7;
+        	i= j+k;
+        	if (i != 7)
+                k = 0;
+        	else
+                j=j-1;
+        	i=0; 
+    }
+}
---
+++ laboratorio/ijvm/template.java
@@ -0,0 +1,14 @@
+
+public class template {
+
+	public static void main(String[] args){
+		    // inizializzazione variabili locali
+
+
+			// codice
+
+
+		 }
+}
+
+
